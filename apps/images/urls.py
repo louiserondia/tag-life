cf769e3280acf003304db2726877f1f63c077fbf
Louise Rondia <louiserondia@gmail.com>
2024-07-04 00:19:40 +0200

    bazar de sass là

--- apps/images/urls.py
+++ apps/images/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 from . import views
-from . import jsonManager
 from . import tagManager
 
 urlpatterns = [
-    path('', views.import_images, name='import_images'),
+    path('images', views.import_images, name='import_images'),
     path('add_random_tag/<str:imageId>/', tagManager.add_random_tag, name='add_random_tag'),
 ]
--- taglife/urls.py
+++ taglife/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('', 'templates/base.html')
+    path('', include('apps.main.urls')),
     path('', include('apps.images.urls'))
 ]
 
